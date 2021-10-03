5b7ca5ee617282bfa630c5f4da909d6ead17de07
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-10-03 15:50:18 +0100

    add style

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Logo = styled.h1`
+  font-family: "Jost", sans-serif;
+  text-transform: uppercase;
+`
+export const LuxuryTextLight = styled.span`
+  font-weight: 400;
+`
